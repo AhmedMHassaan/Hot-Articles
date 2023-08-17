f3b9e0eb370e5b3d547e0364ecd8543248a22f7c
Ahmed <ahmed.m.hassaan1998@gmail.com>
2023-08-17 17:48:16 +0300

    test languageRepository in data layer

--- data/src/androidTest/java/com/ahmedmhassaan/data/repository/LanguageRepositoryImplTest.kt
+++ data/src/androidTest/java/com/ahmedmhassaan/data/repository/LanguageRepositoryImplTest.kt
@@ -1,17 +1,43 @@
 package com.ahmedmhassaan.data.repository
 
-import org.junit.Assert.*
-
-import org.junit.After
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.ahmedmhassaan.data.local.database.LanguageSharedPreferences
+import com.ahmedmhassaan.data.local.datasources.LanguageDataSource
+import com.ahmedmhassaan.domain.repos.LanguageRepository
+import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import org.junit.Before
+import org.junit.Test
 
 class LanguageRepositoryImplTest {
 
+    private lateinit var languageRepository: LanguageRepository
+    private lateinit var dataSource: LanguageDataSource
+    private lateinit var langPref: LanguageSharedPreferences
+    private lateinit var context: Context
+
     @Before
     fun setUp() {
+        context = ApplicationProvider.getApplicationContext<Context?>().applicationContext
+        langPref = LanguageSharedPreferences(context)
+        dataSource = LanguageDataSource(langPref)
+        languageRepository = LanguageRepositoryImpl(dataSource)
     }
 
-    @After
-    fun tearDown() {
+
+    @Test
+    fun testLanguageBeforeSavingNewOne_returnEmptyString(){
+        val lang = languageRepository.getSavedLanguage()
+        assertThat(lang).isEmpty()
     }
+
+    @Test
+    fun test_saveLanguage() {
+        val expectedLang = "abcd"
+        languageRepository.saveLanguage(expectedLang)
+        val lang = languageRepository.getSavedLanguage()
+        assertThat(lang).isEqualTo(expectedLang)
+    }
+
 }
